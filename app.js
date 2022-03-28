6e1a304f61a43c78605298dec81c128e49043fc4
Gareth Everson <gareth@garetheverson.com>
2022-03-28 15:41:22 +0100

    ncnews-2: test typo fix and removing unnecessary dependencies for controller and model per @dentednerd review

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { getTopics } = require('./controllers/topics.controllers');
+
+const app = express();
+app.use(express.json());
+
+// Methods
+app.get('/api/topics', getTopics);
+
+app.all('/*', (req, res) => {
+  res.status(404).send({ msg: 'Route not found' });
+});
+
+// Handle unexpected errors
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: 'Internal server error' });
+});
+
+module.exports = app;
