51ec32cba2fa6d86cd6cb9ecebe0aab8fccd72bf
Gareth Everson <gareth@garetheverson.com>
2022-04-18 19:20:26 +0100

    Allow cross origin resource sharing

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const articlesRouter = require('./routes/articles-router');
 const commentsRouter = require('./routes/comments-router');
 const topicsRouter = require('./routes/topics-router');
 const usersRouter = require('./routes/users-router');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+
+// app.use CORS
+app.use(cors());
 
 // app.use routers
 app.use(apiRouter);
